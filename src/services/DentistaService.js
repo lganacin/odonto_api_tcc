cd77222e666ad364c0ad714a171f0de6ea25357b
Yuri Alexandre Pires de Carvalho <yuri.apcarvalho@gmail.com>
2023-03-12 21:50:39 -0400

    ajuste finais

--- src/services/DentistaService.js
+++ src/services/DentistaService.js
@@ -1,10 +1,14 @@
 const dentistaModel = require("./../models/Dentista");
 const usuarioModel = require("./../models/Usuario");
+const usuarioService = require('../services/UsuarioService')
+
+const mongoose = require('mongoose');
+
 
 module.exports = {
   buscarTodos: async () => {
     try {
-      return await dentistaModel.find({});
+      return await dentistaModel.find({}).populate('usuario').exec();
     } catch (error) {
       throw { message: error.message, status: 500 };
     }
@@ -24,7 +28,7 @@ module.exports = {
         dentista.usuario.role = "Dentista"
         let usuario = await usuarioModel.create(dentista.usuario)
         dentista.usuario = usuario
-        
+
         return await dentistaModel.create(dentista)
       }
       throw { message: "Informações incompletas.", status: 400 };
@@ -35,8 +39,28 @@ module.exports = {
 
   editar: async (id, dentista) => {
     try {
-      if (id && dentista)
-        return await dentistaModel.findOneAndUpdate({ _id: id }, dentista);
+      let usuarioId = null
+      if (id && dentista) {
+        if(dentista.usuario?._id == null && dentista.usuario?.email !== null && dentista.usuario?.senha !== null) {
+          const user = await usuarioService.adicionar({
+            email: dentista.usuario?.email,
+            senha: dentista.usuario?.senha,
+            role: 'Dentista',
+          })
+          usuarioId = user._id
+        }
+
+        if (dentista.usuario?._id != null) {
+          await usuarioService.editar(dentista.usuario?._id, {
+            email: dentista.usuario?.email,
+            senha: dentista.usuario?.senha,
+          })
+        }
+
+        const usuario = dentista.usuario?._id || usuarioId
+        const {_id, ...dados} = dentista
+        return await dentistaModel.findByIdAndUpdate(id, {...dados, usuario});
+      }
       throw { message: "Informações incompletas.", status: 400 };
     } catch (error) {
       throw { message: error.message, status: 500 };
